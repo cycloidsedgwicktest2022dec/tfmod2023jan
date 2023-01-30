fa0fa2c3d1fcb067ff6e7226fe35dd666ea9a112
cycloidsedgwicktest2022dec <120516063+cycloidsedgwicktest2022dec@users.noreply.github.com>
2023-01-30 15:13:54 -0500

    Add files via upload
    WIP module built from hashicorp example, with network hard coded for testing.

---
+++ vm/outputs.tf
@@ -0,0 +1,7 @@
+output "resource_id" {
+  value = azurerm_linux_virtual_machine.example.id
+}
+
+output "name" {
+  value = azurerm_linux_virtual_machine.example.name
+}
---
+++ vm/variable.tf
@@ -0,0 +1,21 @@
+variable "org_short" {
+  type    = string
+  default = "TST"
+}
+
+variable "project" {
+  type    = string
+  default = "HelloWorld"
+}
+
+variable "ResourceGroup" {
+  type    = string
+  default = "TST_eus_network_spoke00_rg"
+}
+
+variable "pubkey" {
+  type    = string
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC9fBHXiMjXWcjmgDv5wAF7YIBySI2Sa9ChpKf5MDicIug3GYO2b6xJRAaQbdZeqIno+iYLQ/n6nt6PaGqKdcRtpeCh8oDyzc++qIqiOJY6EDi5u6pObO2BGE3d5eD8ZzhLNg2kc5FFzRZ1KDllai4F4PdCE5wOMe8kCgG/yb5RlGF4yxZ3XDpQUhlBR1e/29q2V9uyk92D3PCil7vlS1gupbR59aOqP1jZvx0tokZkama5gjWfHdauq+Djrm2hXJQyjWfhtZoE0E4V/XwB6dA24dNYlin5f+flkDOubuJV45V5Xefoo/Izkj6hwOS8fKUiKe7tfaG36M8HfFyGL/jt"
+}
+
+
